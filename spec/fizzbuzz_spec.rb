821b9bb12f93534ea0692e8abe44f2dd85ce92d6
Valentina <maggio.vale@gmail.com>
2022-02-09 11:22:54 +0000

    Tests  passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,13 @@
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
+    it 'returns "fizz" when passed multiple of 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'returns "buzz" when passed multiple of 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'returns "fizzbuzz" when passed multiple of both 3 & 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
 end
