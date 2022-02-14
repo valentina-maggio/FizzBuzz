970e776ea82a8fd02c53139f094cafe60b13ce39
Valentina <maggio.vale@gmail.com>
2022-02-14 11:40:57 +0000

    Code refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,6 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-      "fizzbuzz"
-    elsif number % 3 == 0
-      "fizz"
-    elsif number % 5 == 0
-      "buzz"
-    else
-      number
-    end 
+    return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
+    return "fizz" if number % 3 == 0
+    return "buzz" if number % 5 == 0
+    return number
 end
